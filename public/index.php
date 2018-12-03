186a1e7dff5b51157a7637037e9a1db60d674c59
Tasnádi Zsolt <tasi@syrakuza.hu>
2018-12-03 13:06:27 +0100

    add pdo

--- public/index.php
+++ public/index.php
@@ -4,43 +4,49 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
+$dsn = 'mysql:host=127.0.0.1;dbname=videoteka';
+$usr = 'root';
+$pwd = '';
+$table = 'users';
+
+$pdo = new \Slim\PDO\Database($dsn, $usr, $pwd);
 $app = new \Slim\App;
 
-$GLOBALS['dogList'] = array(
-  array(
-    'id' => 'dog001', // unique ID
-    'name' => 'Napolyi Masztif', // species
-    'size' => 'large', // small, medium, large
-    'place' => 'outdoor' // indoor, outdoor
-  )
-);
-
-$app->get('/dogs', function (Request $request, Response $response, array $args) {
-  return $response->withJson($GLOBALS['dogList']);
+$app->get('/resource', function (Request $request, Response $response, array $args) {
+  global $pdo, $table;
+  $query = $pdo->select()->from($table)->execute();
+  $users = [];
+  while($row = $query->fetch()) {	
+    $users[] = $row;
+  }
+  return $response->withJson($users);
 });
 
-$app->get('/dogs/show/{id}', function (Request $request, Response $response, array $args) {
-  $index = array_search($args['id'], array_column($GLOBALS['dogList'], 'id'));
-  $element = $GLOBALS['dogList'][$index];
-  return $response->withJson($element);
+$app->get('/resource/{id}', function (Request $request, Response $response, array $args) {
+  global $pdo, $table;
+  $query = $pdo->select()->from($table)->where('id', '=', $args['id'])->execute();
+  $user = $query->fetch();
+  return $response->withJson($user);
 });
 
-$app->post('/dogs', function (Request $request, Response $response, array $args) {
-  $params = $request->getQueryParams();
-  array_push($GLOBALS['dogList'], $params);
-  return $response->withJson($GLOBALS['dogList']);
+$app->post('/resource', function (Request $request, Response $response, array $args) {
+  global $pdo, $table;
+  $body = $request->getParsedBody();
+  $newId = $pdo->insert(array_keys($body))->into($table)->values(array_values($body))->execute();
+  return $response->withJson(['created' =>  TRUE]);
 });
 
-$app->put('/dogs/update/{id}', function (Request $request, Response $response, array $args) {
-  $index = array_search($args['id'], array_column($GLOBALS['dogList'], 'id'));
-  $element = $GLOBALS['dogList'][$index];
-  return $response->withJson($element);
+$app->put('/resource/{id}', function (Request $request, Response $response, array $args) {
+  global $pdo, $table;
+  $body = $request->getParsedBody();
+  $pdo->update($body)->table($table)->where('id', '=', $args['id'])->execute();
+  return $response->withJson(['updated' =>  TRUE]);
 });
 
-$app->delete('/dogs/{:id}', function (Request $request, Response $response, array $args) {
-  $index = array_search($args['id'], array_column($GLOBALS['dogList'], 'id'));
-  $element = $GLOBALS['dogList'][$index];
-  return $response->withJson($GLOBALS['dogList']);
+$app->delete('/resource/{id}', function (Request $request, Response $response, array $args) {
+  global $pdo, $table;
+  $pdo->delete()->from($table)->where('id', '=', $args['id'])->execute();
+  return $response->withJson(['deleted' =>  TRUE]);
 });
 
 $app->run();
